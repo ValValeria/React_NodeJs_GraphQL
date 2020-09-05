703d6e7785e7aeeaf7e4d80b2f6638f1cf7d80a8
ValValeria <led2017l@gmail.com>
2020-09-05 14:09:36 +0300

    fix

--- client/js/Pages/Main.jsx
+++ client/js/Pages/Main.jsx
@@ -4,22 +4,36 @@ import Footer from '../Components/Footer'
 import Form from '../Components/Form'
 import {connect} from 'react-redux'
 import Chip from '@material-ui/core/Chip';
+import Rating from '@material-ui/lab/Rating';
+import Box from '@material-ui/core/Box';
 import Card from '@material-ui/core/Card'
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
+import _ from 'lodash';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
 import Typography from '@material-ui/core/Typography';
-import _ from 'lodash'
 
-const maptostate = (state) => ({data:state.whyme,projects:state.data})
+function Icon(){
+    return (
+        <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-arrow-down-square-fill" fill="#f8de15" xmlns="http://www.w3.org/2000/svg">
+          <path fillRule="evenodd" d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"/>
+        </svg>
+        )
+}
+
+
+const maptostate = (state) => ({data:state.whyme,projects:state.data,questions:state.questions})
 
 function createMarkup(html) {
     return {__html: html};
   }
 
-function Main({data,projects}){
+function Main({data,projects,questions}){
 
     return (
         <>
@@ -112,15 +126,60 @@ function Main({data,projects}){
             </div>
          </section>
 
-        <section>
+        <section style={{paddingBottom:"0"}}>
              <div className="wrap">
                <h5 className="headline text-center">My reviews</h5>
+               <div className="review container-md">
+                <div className="card m-auto d-flex flex-row align-items-center shadows_b">
+                   <div className="card-body">
+                        <h5 className="card-title">Max Novitskiy</h5>
+                        <p className="card-text"><small className="text-muted">UI designer</small></p>
+                        <p className="card-text">
+                        За время сотрудничества показатели по продвижению нашего сайта демонстрируeт только положительную динамику. Разработчик показал высокие деловые стандарты, индивидуальный подход, поиск оптимальных решений для достижения поставленных целей.
+                        </p>
+                   </div>
+                   <div className="stars">
+                     <Box component="fieldset" mb={1} borderColor="transparent">
+                        <Typography component="legend">Read only</Typography>
+                        <Rating name="read-only" value={4} readOnly />
+                     </Box>
+                   </div>
+                </div>
+                 <div className="m-auto mt-30 mrt_4_rem"><img src="/img/image-4.jpg" alt="" style={{objectFit:"contain",width:"100%"}}/></div>
+               </div>
              </div>
         </section>
         
-        <section>
+        <section id="questions">
             <div className="wrap">
                <h5 className="headline text-center">Questions</h5>
+               <div className="d-flex flex__wrap__700 ccl">
+                  <div className="shadow_b " style={{margin:"20px",paddingTop:"30px"}}>
+                       {
+                       questions.map(elem=>{
+                           return (
+                            <Accordion key={elem}>
+                            <AccordionSummary
+                              expandIcon={<Icon/>}
+                              aria-controls="panel1a-content"
+                              id="panel1a-header"
+                            >
+                              <Typography >{elem[0]}</Typography>
+                            </AccordionSummary>
+                            <AccordionDetails>
+                              <Typography>
+                                {elem[1]}
+                              </Typography>
+                            </AccordionDetails>
+                          </Accordion>
+                           )
+                       })
+                       }
+               </div>
+                <div className="image m-20 img-container img__700__center" style={{margin:"20px"}}>
+                      <img src="/img/bg-slide.jpg" alt="" />
+                </div>
+               </div>
              </div>
         </section>
          <div className="triangle-block">
